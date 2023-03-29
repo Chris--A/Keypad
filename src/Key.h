a81aa0fb204798645b4f6f8641e4d87a38420f9c
Djair Guilherme <djair.jr@gmail.com>
2023-03-29 20:02:42 -0300

    Update Key.h

--- src/Key.h
+++ src/Key.h
@@ -34,8 +34,8 @@
 
 #include <Arduino.h>
 
-#define OPEN LOW
-#define CLOSED HIGH
+#define KEYPAD_OPEN LOW
+#define KEYPAD_CLOSED HIGH
 
 typedef unsigned int uint;
 typedef enum{ IDLE, PRESSED, HOLD, RELEASED } KeyState;
